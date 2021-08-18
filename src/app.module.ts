3d81f61473df8e101c8458f7b2971183bf43e785
Kiran K K <kirankalak4@gmail.com>
2021-08-19 02:26:31 +0530

    2. Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,14 @@
+// This is Module
+// Module is the meeting place of  controllers and services
+// We can add all the controllers and services/providers as 
+// dependencies here.
+
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ItemsController } from './Items/items/items.controller';
+
+
 
 @Module({
   imports: [],
